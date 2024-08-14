8e3493876a8c96bf0993fa374c4ffd27e356702c
root <root@linux-vm.5ba5hv4n45rubfwjp3eszgwq5f.cx.internal.cloudapp.net>
2024-08-14 15:15:24 +0000

    rg

---
+++ resourcegrp/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features = {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.40.0"
+    }
+    random = {
+      version = ">=3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.34.0"
+    }
+  }
+
+}
+
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "john"
+  location = "East US"
+}
+
