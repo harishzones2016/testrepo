3a337f9d134aee486e93167f4f044bdd642c379c
harishzones2016 <107680110+harishzones2016@users.noreply.github.com>
2024-08-18 21:12:23 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,12 +1,22 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.3.0"
+      version = ">= 3.40.0"
+    }
+    random = {
+      version = ">=3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.34.0"
     }
   }
+
 }
+
+provider "azurerm" {
+
+  features {}
+}
+
