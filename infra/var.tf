207f69179f7a7bc9e0d9284d55be66fdeb7f8e82
harishzones2016 <107680110+harishzones2016@users.noreply.github.com>
2024-08-18 21:08:55 +0530

    Update var.tf

--- infra/var.tf
+++ infra/var.tf
@@ -7,22 +7,16 @@ variable "dns_prefix" {
 }
 
 variable "cluster_name" {
-  default = "aks-k8s-2022"
-}
+ }
 
-variable "kubernetes_version" {
-  default = "1.21.2"
-}
 
 variable "acr_name" {
   type = string
-  default = "acr42001"
+ 
 }
-
 
 variable "resource_group_name" {
-  
-}
+  }
 
 variable "location" {
   default = "westus"
