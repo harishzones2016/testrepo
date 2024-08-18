e74225e92b272a1ccf7bf4f3992c78d76ff8d240
root <root@linux-vm.5ba5hv4n45rubfwjp3eszgwq5f.cx.internal.cloudapp.net>
2024-08-18 14:09:53 +0000

    aks

---
+++ infra/main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+#  kubernetes_version  = var.kubernetes_version
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    Environment = "Production"
+  }
+
+
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true # false
+}
+
+data "azurerm_client_config" "current" {
+}
+
+data "azurerm_user_assigned_identity" "identity" {
+  name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
+  resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group
+}
+
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
+  skip_service_principal_aad_check = true
+}
